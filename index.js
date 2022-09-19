e1d83f68950fb82a2e949936ce81f46db65c5267
Haki <defvermillion@gmail.com>
2022-09-19 20:29:43 +0300

    azxczxczxc

--- index.js
+++ index.js
@@ -204,22 +204,12 @@ const restorantData = {
     openNow: true
 };
 
-function isOpen(prop) {
-    let answer = '';
-    prop ? answer = 'Открыто' : answer = 'Закрыто';
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
 
-    return answer;
+    copy.waitors = [{name: 'Mike', age: 32}];
+    return copy;
 }
 
-console.log(isOpen(restorantData.openNow));
-
-
-function isAverageLunchPriceTrue(fDish, sDish, average) {
-    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < +average.slice(0, -1)) {
-        return 'Цена ниже средней'; 
-    } else {
-        return 'Цена выше средней';
-    }
-}
-w414124ssdfsd
-console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+console.log(transferWaitors(restorantData));
+afasfsf
